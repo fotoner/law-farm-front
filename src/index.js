51d2289a734bdcd35b1e9f6b59bb046e24611fb8
Hyeonjae Nam <jaemuon5582@gmail.com>
2021-10-12 00:26:16 +0900

    merge 용

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import Root from "./Root";
 import "./css/core.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    {/* <Provider> */}
       <Root />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
